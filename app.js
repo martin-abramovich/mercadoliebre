3011b1153a27c4df42dfc1590f7c225547dfdb66
Martin Abramovich <abramovich.martin@gmail.com>
2023-06-04 18:54:02 -0300

    enunciados 1 2 3 4

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile((__dirname + '/views/home.html'));
+})
+
+app.listen(3000, function() {
+    console.log("Servidor corriendo");
+})
